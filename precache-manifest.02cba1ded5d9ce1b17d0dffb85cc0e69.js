e80401143bef9fdd1f101e3b2ae0d910769d9fd4
Darren <d-pierre90@hotmail.com>
2020-01-09 21:40:53 -0500

    Updates

---
+++ precache-manifest.02cba1ded5d9ce1b17d0dffb85cc0e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae7e3b7c0b16459c8a003c7b4a9a4e5",
+    "url": "/Digital-Academy-Digital-Business-Cards/index.html"
+  },
+  {
+    "revision": "0d34818ff55976842b3c",
+    "url": "/Digital-Academy-Digital-Business-Cards/static/css/main.e23e5d77.chunk.css"
+  },
+  {
+    "revision": "6864e8b171f7e44ef4ea",
+    "url": "/Digital-Academy-Digital-Business-Cards/static/js/2.ba325e66.chunk.js"
+  },
+  {
+    "revision": "6152fcca88d2cd378559f297d79667d6",
+    "url": "/Digital-Academy-Digital-Business-Cards/static/js/2.ba325e66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d34818ff55976842b3c",
+    "url": "/Digital-Academy-Digital-Business-Cards/static/js/main.e5338ad3.chunk.js"
+  },
+  {
+    "revision": "581543de9a8df8601c05",
+    "url": "/Digital-Academy-Digital-Business-Cards/static/js/runtime-main.f9c3ce5d.js"
+  }
+]);
